6e0ddf5400c777409250453c11e5f68674b88ce0
joyliu-q <joyliu.q@gmail.com>
2021-07-03 17:40:32 -0700

    :new: Add example usage for in_toto_run

---
+++ examples/in_toto_run_test.rs
@@ -0,0 +1,20 @@
+use in_toto::crypto::PrivateKey;
+use in_toto::runlib::in_toto_run;
+
+const ED25519_1_PRIVATE_KEY: &'static [u8] = include_bytes!("../tests/ed25519/ed25519-1");
+
+fn main() {
+    let key = PrivateKey::from_ed25519(ED25519_1_PRIVATE_KEY).unwrap();
+
+    let link = in_toto_run(
+        "example",
+        Some("tests"),
+        &["tests/test_runlib"],
+        &["tests/test_runlib"],
+        &["sh", "-c", "echo 'in_toto says hi' >> hello_intoto"],
+        Some(key),
+    )
+    .unwrap();
+
+    println!("Generated link: {:?}", link)
+}
